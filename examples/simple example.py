59a0296dda420e02b30bfd73374ccd1207fb35b9
Dane Finlay <Danesprite@gmail.com>
2017-11-11 23:58:38 +1100

    Make simple example more similar to the example in the README

--- examples/simple example.py
+++ examples/simple example.py
@@ -10,19 +10,19 @@ def main():
     rule = PublicRule("hello", "hello world")
 
     # Create a grammar and add the new rule to it
-    grammar = Grammar("g")
+    grammar = Grammar()
     grammar.add_rule(rule)
 
     # Compile the grammar using compile_grammar()
-    print("Grammar '%s' compiles to:" % grammar.name)
+    # compile_to_file(file_path) may be used to write a compiled grammar to
+    # a file instead.
+    # Compilation is not required for finding matching rules.
     print(grammar.compile_grammar())
 
-    # Find rules in the grammar that match 'hello world'
-    speech = "hello world"
-    matching = grammar.find_matching_rules(speech)
-    print("The following rules matched '%s':" % speech)
-    for match in matching:
-        print(match)
+    # Find rules in the grammar that match 'hello world'.
+    matching = grammar.find_matching_rules("hello world")
+    print("Matching: %s" % matching[0])
+
 
 if __name__ == '__main__':
     main()
