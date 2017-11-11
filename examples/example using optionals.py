9d691fc221d6549665b225c663be170173b76155
Dane Finlay <Danesprite@gmail.com>
2017-11-11 21:42:11 +1100

    Add example to show the use of OptionalGrouping expansions

---
+++ examples/example using optionals.py
@@ -0,0 +1,37 @@
+from jsgf import PublicRule, OptionalGrouping, Grammar, Sequence
+
+
+def print_matching(grammar, speech):
+    matching = grammar.find_matching_rules(speech)
+    if not matching:
+        print("No rules matched '%s'." % speech)
+    else:
+        print("Rules matching '%s':" % speech)
+        for r in matching:
+            print(r)
+    print("")
+
+
+def main():
+    # The Sequence expansion requires all of its children expansions to be spoken
+    # in sequence. The OptionalGrouping expansion optionally requires its child
+    # expansion to be spoken.
+
+    # Create a public rule using an optional expansion
+    rule = PublicRule("greet", Sequence("hey", OptionalGrouping("there")))
+
+    # Create a grammar and add the new rule to it
+    grammar = Grammar("g")
+    grammar.add_rule(rule)
+
+    # Compile the grammar using compile_grammar()
+    print("Grammar '%s' compiles to:" % grammar.name)
+    print(grammar.compile_grammar())
+
+    # Use or do not use the optional word 'there'
+    print_matching(grammar, "hey")
+    print_matching(grammar, "hey there")
+
+
+if __name__ == '__main__':
+    main()
