ba6df9f75b5825f5dcd5e51888dd728a2166184b
Dane Finlay <Danesprite@gmail.com>
2017-11-21 21:55:09 +1100

    Combine tests for properties of the Rule class into one TestCase class

--- test/test_rules.py
+++ test/test_rules.py
@@ -4,17 +4,17 @@ from jsgf import *
 from jsgf.rules import CompilationError
 
 
-class RuleDependenciesCase(unittest.TestCase):
+class PropertiesTests(unittest.TestCase):
     """
-    Test the 'dependencies' property of the Rule class.
+    Test some properties of the Rule class.
     """
-    def test_simple(self):
+    def test_dependencies_simple(self):
         rule2 = HiddenRule("greetWord", AlternativeSet("hello", "hi"))
         rule3 = HiddenRule("name", AlternativeSet("peter", "john", "mary", "anna"))
         rule1 = PublicRule("greet", RequiredGrouping(RuleRef(rule2), RuleRef(rule3)))
         self.assertSetEqual(rule1.dependencies, {rule2, rule3})
 
-    def test_complex(self):
+    def test_dependencies_complex(self):
         rule2 = HiddenRule("greetWord", AlternativeSet("hello", "hi"))
         rule3 = HiddenRule("firstName", AlternativeSet("peter", "john", "mary", "anna"))
         rule4 = HiddenRule("lastName", AlternativeSet("smith", "ryan", "king", "turner"))
@@ -23,26 +23,20 @@ class RuleDependenciesCase(unittest.TestCase):
         rule1 = PublicRule("greet", RequiredGrouping(RuleRef(rule2), RuleRef(rule5)))
         self.assertSetEqual(rule1.dependencies, {rule2, rule3, rule4, rule5})
 
-
-class RuleRefCount(unittest.TestCase):
-    """
-    Test the 'reference_count' property of the Rule class.
-    """
-    def setUp(self):
-        self.rule = HiddenRule("greet", "hello")
-
-    def test_simple(self):
-        rule_refs = [RuleRef(self.rule)]
-        self.assertEqual(1, self.rule.reference_count)
+    def test_reference_count_simple(self):
+        r = HiddenRule("greet", "hello")
+        rule_refs = [RuleRef(r)]
+        self.assertEqual(1, r.reference_count)
         rule_refs.pop()
-        self.assertEqual(0, self.rule.reference_count)
+        self.assertEqual(0, r.reference_count)
 
-    def test_with_grammar(self):
+    def test_reference_count_with_grammar(self):
         grammar = Grammar()
-        grammar.add_rule(self.rule)
-        self.assertEqual(0, self.rule.reference_count, "rule '%s' is not independent.")
-        grammar.add_rule(PublicRule("test", RuleRef(self.rule)))
-        self.assertEqual(1, self.rule.reference_count, "rule '%s' is independent.")
+        r = HiddenRule("greet", "hello")
+        grammar.add_rule(r)
+        self.assertEqual(0, r.reference_count, "rule '%s' is not independent.")
+        grammar.add_rule(PublicRule("test", RuleRef(r)))
+        self.assertEqual(1, r.reference_count, "rule '%s' is independent.")
 
 
 class OptionalOnlyRule(unittest.TestCase):
