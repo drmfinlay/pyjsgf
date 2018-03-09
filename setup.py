dbc833ccf875b320f1ea7c6d206768727a833e35
Dane Finlay <Danesprite@gmail.com>
2018-03-10 00:20:56 +1100

    Change setup.py to use setuptools instead of distutils
    This is necessary to automatically install the pyparsing dependency.
    
    Due to the different way that setuptools stores installed packages, the
    distutils version of pyjsgf will need to be uninstalled for Python to
    use the newer version.

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-      name='pyjsgf',
-      description='JSpeech Grammar Format (JSGF) compiler and matcher for Python',
-      long_description="""
-            pyjsgf can be used to construct JSGF rules and grammars, compile them into 
-            strings or files, and match speech text to the matching rule objects used 
-            to compile the grammars. Speech text can be from a recognition engine's 
-            speech hypothesis, for example.
-      """,
-      maintainer='Dane Finlay',
-      version='1.0',
-      packages=['jsgf', 'jsgf.ext'])
+    name='pyjsgf',
+    description='Java Speech Grammar Format (JSGF) compiler and matcher for Python',
+    long_description="""
+        pyjsgf can be used to construct JSGF rules and grammars, compile them into 
+        strings or files, and match speech text to the matching rule objects used 
+        to compile the grammars. Speech text can be from a recognition engine's 
+        speech hypothesis, for example.
+    """,
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: OS Independent",
+        "Topic :: Multimedia :: Sound/Audio :: Speech",
+        "Topic :: Software Development :: Libraries",
+    ],
+    author='Dane Finlay',
+    author_email='Danesprite@gmail.com',
+    version='1.0.1',
+    packages=['jsgf', 'jsgf.ext'],
+    install_requires=["pyparsing"]
+)
