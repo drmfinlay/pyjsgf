45818acb0eed40ef2e54e4ab00e9afdf32bd8e55
Dane Finlay <Danesprite@gmail.com>
2018-06-14 17:44:41 +1000

    Update to version 1.3.0
    - Fix various bugs with JSGF expansions.
    - Add methods/properties to Rule and Grammar classes for JSGF tag
      support.
    - Add rule resolution for NamedRuleRef class.
    - Add method and property for checking expansion match values for
      each repetition.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     ],
     author='Dane Finlay',
     author_email='Danesprite@gmail.com',
-    version='1.2.3',
+    version='1.3.0',
     packages=['jsgf', 'jsgf.ext'],
     install_requires=["pyparsing", "six"]
 )
