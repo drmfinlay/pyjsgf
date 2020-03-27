2dd234ef4ea54ab0c7d4f470384872796d68849b
Dane Finlay <Danesprite@posteo.net>
2020-03-28 10:45:32 +1100

    Update import resolution unit tests
    - Define test grammar using parse_grammar_string(). This is more
      readable and should match what parse_grammar_file() returns
      during import resolution.
    - Split test_resolve() into multiple test methods with concise
      docstrings.

--- test/test_importing.py
+++ test/test_importing.py
@@ -19,15 +19,15 @@ class ImportResolutionCase(unittest.TestCase):
         elif not (cls.cwd.endswith("test") or os.path.isdir("grammars")):
             assert False, "tests are running from an unusual working directory"
 
-        # Define expected file grammars here.
+        # Parse test file grammars.
         class Grammars(object):
-            # grammars.test1.jsgf
-            test1 = Grammar("grammars.test1")
-            test1.add_rules(
-                Rule("X", False, "x"),
-                Rule("Y", False, "y"),
-                PublicRule("Z", AlternativeSet(NamedRuleRef("X"), NamedRuleRef("Y")))
-            )
+            test1 = parse_grammar_string("""
+                #JSGF V1.0;
+                grammar grammars.test1;
+                <X> = x;
+                <Y> = y;
+                public <Z> = <X>|<Y>;
+            """)
 
         cls.grammars = Grammars
 
@@ -40,55 +40,53 @@ class ImportResolutionCase(unittest.TestCase):
 class ImportClassCase(ImportResolutionCase):
     """ Import class tests. """
 
-    def test_resolve(self):
-        """ Test that the Import.resolve() method works correctly. """
-        import1 = Import("grammars.test1.Z")
-        expected_grammar = self.grammars.test1
-        Z, = expected_grammar.get_rules("Z")
-
+    def test_resolve_single(self):
+        """ Import.resolve() correctly handles importing single rules. """
         # Check that import.resolve() returns the 'Z' rule and updates the memo
         # dictionary.
+        expected_grammar = self.grammars.test1
+        Z, = expected_grammar.get_rules("Z")
         memo = {}
-        self.assertEqual(import1.resolve(memo), Z)
+        self.assertEqual(Import("grammars.test1.Z").resolve(memo), Z)
         self.assertDictEqual(memo, {
             "grammars.test1": expected_grammar,
             "grammars.test1.Z": Z
         })
 
-        # Test that wild card imports work correctly.
-        import2 = Import("grammars.test1.*")
-
-        # Check that import.resolve() returns all grammar rules and re-uses the
-        # objects in the memo dictionary.
-        grammar_parsed_by_import1 = memo["grammars.test1"]
-        self.assertEqual(import2.resolve(memo), [Z])
-        self.assertIs(memo["grammars.test1"], grammar_parsed_by_import1)
+    def test_resolve_wildcard(self):
+        """ Import.resolve() correctly handles wildcard import statements. """
+        expected_grammar = self.grammars.test1
+        Z, = expected_grammar.get_rules("Z")
+        memo = {}
+        self.assertEqual(Import("grammars.test1.*").resolve(memo), [Z])
         self.assertDictEqual(memo, {
             "grammars.test1": expected_grammar,
             "grammars.test1.Z": Z,
             "grammars.test1.*": [Z]
         })
 
+    def test_resolve_memo_dictionary_reused(self):
+        """ Import.resolve() reuses objects in the memo dictionary. """
+        memo = {}
+        Import("grammars.test1.Z").resolve(memo)
+        import1_grammar = memo["grammars.test1"]
+        import1_z = memo["grammars.test1.Z"]
+        self.assertIs(Import("grammars.test1.Z").resolve(memo),
+                      import1_z)
+        self.assertIs(memo["grammars.test1"], import1_grammar)
+
     def test_resolve_non_existant_grammars(self):
-        """
-        Test that errors are raised when resolving import statements for
-        non-existent grammars.
-        """
+        """ Resolving import statements for non-existent grammars raises errors. """
         self.assertRaises(JSGFImportError, Import("grammars.test0.*").resolve)
         self.assertRaises(JSGFImportError, Import("grammars.test0.X").resolve)
 
     def test_resolve_non_existant_rule(self):
-        """
-        Test that an error is raised when resolving an import statement for a rule
-        that doesn't exist in a grammar.
+        """ Resolving import statements for non-existent grammar rules raises errors.
         """
         self.assertRaises(JSGFImportError, Import("grammars.test1.A").resolve)
 
     def test_resolve_private_rules(self):
-        """
-        Test that an error is raised when resolving an import statement for a rule
-        that exists in a grammar, but is private (visible=False).
-        """
+        """ Resolving import statements for private grammar rules raises errors. """
         self.assertRaises(JSGFImportError, Import("grammars.test1.X").resolve)
 
     def test_hash_cmp(self):
